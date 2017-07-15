1b6875d934915b5f5fa275d04120b7b1165628f1
Yu-Zhi Chen <kywk.id@gmail.com>
2017-07-15 10:48:56 +0800

    First implement for sloth.js runner

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# sloth
-deploy runner with scripts &amp; templates
+sloth.js: Spitz 部屬工具 
+=========================
+
+sloth.js 是 Spitz 的部屬和更新工具，管理與批次執行系統更新程序。  
+內建常用的更新部屬模組，可直接被引用執行；
+亦可透過擴充 Script 來客製處理版本間的更新程序。
+
+使用說明
+--------
+
+```bash
+  Usage: sloth [options]
+
+  Options:
+
+    -h, --help             output usage information
+    -V, --version          output the version number
+    -p, --path <folder>    apply update jobs from <folder>
+    -u, --update <a>..<b>  apply serial update jobs from <begin> to <end>
+    -i, --init             apply init script after update
+    -t, --target <name>    specify effect target, default: null
+
+  Examples:
+
+    $ ./sloth.js -p 160327 -t mirror
+    $ node sloth.js -u 160327..160430 --init -t master
+```
+
+
+sloth.js 檔案結構
+------------------
+
+sloth.js 檔案結構如下
+{% asciitree %}
+config
+-<TARGET>
+--db.json (#1)
+--...etc...
+-db.json (#1)
+-...etc...
+lib
+-builtin-module.js
+-builtin-script.sh
+-...etc...
+update_<Number>
+-config
+--db.json (#1)
+--...etc...
+-<TARGET>
+--config
+---db.json (#1)
+---...etc...
+--data.txt
+--...etc...
+-00_js-script-here.js
+-10_apply-sql.sql
+-20_builtin-js-script.json
+-30_builtin-shell-script.json
+-40_bash-script-here.sh
+update_<Number>
+sloth.js
+README.md
+{% endasciitree %}
+
+### 檔案結構說明 ###
+
+1.  設定檔複寫資訊
+
+
+支援格式
+--------
+
+### JSON (*.json) ###
+
+### JavaScript (*.js) ###
+
+### Bash Script (*.sh) ###
+
+### SQL (*.sql) ###
+
+
+
+
+內建模組
+--------
+
+### src_depoly ###
+
+
+
+備註
+----
+
+### 參考資料 References ###
+
+__Used Packages__
+-   [commander](https://www.npmjs.com/package/commander)
+-   [inquirer](https://www.npmjs.com/package/inquirer)
+
+__References__
+-   [Command-line utilities with Node.js](http://goo.gl/9zPKct)
+-   [Building command line tools with Node.js - Atlassian Developers](https://goo.gl/mSJCrC)
+-   [Node.js for Command-line Developers | Chegg Engineering](http://goo.gl/Clf9nI)
+-   [Creating Node.js Command Line Utilities to Improve Your Workflow -Telerik Developer Network](http://goo.gl/AKtVY1)
---
+++ sloth.js
@@ -0,0 +1,216 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+var fs = require('fs');
+var async = require('async');
+var sloth = require('commander');
+var chalk = require('chalk');
+
+
+/**
+ * JS common functions
+ */
+function sortNumber (a, b) {
+  return a - b;
+}
+
+/**
+ * sloth.js: Util functions
+ */
+function getDirObj (path) {
+  var obj = {};
+  try {
+    path = path.match(/\/$/) ? path : path + '/';
+    var _pstats = fs.statSync(path);
+    if (_pstats.isDirectory()) {
+      var _files = fs.readdirSync(path);
+      for (var i = 0; i < _files.length; i++) {
+        if (_files[i].match(/.?json$/))
+          obj[_files[i].split(/(.?)json$/)[0]] = require(path + _files[i]);
+      }
+    }
+  } catch (e) {}
+  return obj;
+}
+
+function getDirFiles (path) {
+  var files = [];
+  try {
+    path = path.match(/\/$/) ? path : path + '/';
+    var _pstats = fs.statSync(path);
+    if (_pstats.isDirectory()) {
+      var _files = fs.readdirSync(path);
+      for (var i = 0; i < _files.length; i++) {
+        if (_files[i].match(/.?\.(js|json|sql|sh)$/))
+          files.push(_files[i]);
+      }
+    }
+  } catch (e) {}
+  return files;
+}
+
+
+/**
+ * sloth.js: Prepare CLI information
+ */
+function helpMsg (msg, type) {
+  var color = {
+    'i': 'cyan',
+    'w': 'yellow',
+    'e': 'red'
+  };
+  var prefix = {
+    'i': 'Info: ',
+    'w': 'Warn: ',
+    'e': 'Error: '
+  };
+
+  if (msg) {
+    console.log('');
+    console.log(chalk.bold(chalk[color[type]]('  ' + prefix[type] + msg)));
+  }
+  return sloth.help();
+}
+
+function range (val) {
+  return val.split('..').map(Number);
+}
+
+function list (val) {
+  return val.split(',');
+}
+
+
+sloth.version('0.0.1')
+  .option('-p, --path <folder>', 'apply update jobs from <folder>')
+  .option('-u, --update <a>..<b>', 'apply serial update jobs from <begin> to <end>', range)
+  .option('-i, --init', 'apply init script after update')
+  .option('-t, --target <name>', 'specify effect target, default: null');
+  // .option('-s, --step', 'step by step interactive mode')
+  // .option('-f, --force', 'force finish all task, ignore error occurs');
+
+sloth.on('--help', function () {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ ./sloth.js -p 160327 -t mirror');
+  console.log('    $ node sloth.js -u 160327..160430 --init -t master');
+  console.log('');
+});
+
+sloth.parse(process.argv);
+
+
+/**
+ * Main flow
+ * - Generate source folder list
+ * - Create config & target info for each source folder
+ * - Generate job list with mapped config, target
+ * - Execute & log each job
+ */
+var config = {};
+var target = {};
+
+var src_queue = [];
+var job_queue = [];
+var log_queue = [];
+
+
+// 1. Generate source folder list
+//    - check and parse argumants
+//    - check path exists
+//    - add to source folder list
+if (sloth.path) {
+  var _path = sloth.path.match(/update_(\d*)/) ? sloth.path : 'update_' + sloth.path;
+  try {
+    var _pstats = fs.statSync(_path);
+    if (_pstats.isDirectory()) {
+      src_queue.push(_path);
+    } else {
+      helpMsg('Path is not a folder', 'e');
+    }
+  } catch (e) {
+    helpMsg('Path not exist.', 'e');
+  }
+} else if (sloth.update) {
+  var _files = fs.readdirSync('.');
+  var _dirs = [];
+  for (var i = 0; i < _files.length; i++) {
+    if (_files[i].match(/update_(\d*)/)) {
+      var _num = parseInt(_files[i].split(/update_(\d*)/)[1]);
+      if ((_num >= sloth.update[0]) && (_num <= sloth.update[1]))
+        src_queue.push('update_' + _num);
+    }
+  }
+}
+
+if (!src_queue.length)
+  helpMsg('Please spicify the source folder', 'i');
+
+
+// 2. Create config & target inf
+//    - Create global config info
+//    - Create global target info
+//    - Override config with <TARGET>
+//    - Override config with source/config
+//    - Override config with source/<TARGET>/config
+//    - Override target with source/<TARGET>
+
+config = getDirObj('./config/');
+
+if (sloth.target) {
+  target = getDirObj('./' + sloth.target);
+  var _tconfig = getDirObj('./' + sloth.target + '/config');
+  for (key in _tconfig)
+    config[key] = _tconfig[key];
+}
+
+for (var i = 0; i < src_queue.length; i++) {
+  var _src_config = getDirObj('./' + src_queue[i] + '/config');
+  var _src_target = getDirObj('./' + src_queue[i] + '/' + sloth.target);
+  var _src_tconfig = getDirObj('./' + src_queue[i] + '/' + sloth.target + '/config');
+
+  var _src_obj = {
+    'path': '' + src_queue[i],
+    'config': {},
+    'target': {}
+  };
+
+  for (key in config)
+    _src_obj.config[key] = config[key];
+  for (key in _src_config)
+    _src_obj.config[key] = _src_config[key];
+  for (key in _src_tconfig)
+    _src_obj.config[key] = _src_tconfig[key];
+
+  for (key in target)
+    _src_obj.target[key] = target[key];
+  for (key in _src_target)
+    _src_obj.target[key] = _src_target[key];
+
+  src_queue[i] = _src_obj;
+}
+
+
+// 3. Generate job list with mapped config, target
+//    - read supported files from source folder
+//    - if --init, read init script under source folder
+for (var i = 0; i < src_queue.length; i++) {
+  var _src_files;
+
+  _src_files = getDirFiles(src_queue[i].path);
+  for (var j = 0; j < _src_files.length; j++)
+    job_queue.push({'job': _src_files[j], 'config': src_queue[i].config, 'target': src_queue[i].target});
+
+  if (sloth.init) {
+    _src_files = getDirFiles(src_queue[i].path + '/init');
+    for (var j = 0; j < _src_files.length; j++)
+      job_queue.push({'job': _src_files[j], 'config': src_queue[i].config, 'target': src_queue[i].target});
+  }
+}
+
+console.log(job_queue);
+
+
+// 4.
